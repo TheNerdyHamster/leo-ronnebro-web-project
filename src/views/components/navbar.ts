58408231a491263c1a41ec68ae8b596cd849a830
Leo Rönnebro <leo.ronnebro@nerdyhamster.net>
2021-02-17 23:21:22 +0100

    Basic navbar template

--- src/views/components/navbar.ts
+++ src/views/components/navbar.ts
@@ -1,11 +1,28 @@
 const Navbar = {
     render: async (): Promise<string> => {
         const view = `
-           <nav>
-            <div>
+           <section class="navbar">
+            <div class="logo">
                 <h1>Navbar</h1>
             </div>
-           </nav>
+
+            <nav>
+                <ul class="list">
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Login</a>
+                    </li>
+                    <li>
+                        <a href="#">Register</a>
+                    </li>
+                </ul>
+            </nav>
+           </section>
         `;
 
         return view;
